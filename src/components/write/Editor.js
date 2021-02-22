bc3ead7edf3bd1ef0466064c777bf947acc914fd
박채영 <ggodchae@naver.com>
2021-02-22 21:00:36 +0900

    edit Editor.js

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -43,12 +43,7 @@ const Editor = () => {
       theme: "bubble",
       placeholder: "내용을 작성하세요...",
       modules: {
-        toolbar: [
-          [{ header: "1" }, { header: "2" }],
-          ["bold", "italic", "underline", "strike"],
-          [{ list: "ordered" }, { list: "bullet" }],
-          ["blockquote", "code-block", "link", "image"],
-        ],
+        toolbar: [],
       },
     });
   }, []);
